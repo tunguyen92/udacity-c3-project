dddd1b251d0c87161086660752f6dfb6d2ea2ce4
tunguyen92 <tunguyen3692@gmail.com>
2023-05-04 14:42:26 +0700

    .

---
+++ set_env.sh
@@ -0,0 +1,15 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export AWS_ACCESS_KEY_ID=ASIA2ZX3XYTMFJRJIRBE
+export AWS_SECRET_ACCESS_KEY=c7zmgTzPttkzt7gVuLv3DEewBj5lbv/HhrS6UpIq
+export AWS_SESSION_TOKEN=FwoGZXIvYXdzEJb//////////wEaDJHNwT4llG/jI7BV4CLVAU7LRMtORhH11TYpBglqs/LvINbPHhZyqJdn4caAlDmz44x2dU3jzB37lwkPIO8mc7Z+ARxgJpSuJiV0M+KzN2KmDDZTTt5VEPSJ1bfS/En3s7Jcjv5+KN4Bb0+VJMRV2doaUWNAPUw7KJAh+mPSY0ft8Ni6GzZDbaTWFZ8sEFV0OINwwpd6AkgqqaszQCoCKBK9r5q3nnw+zw6Azk5KO8G/jxiA5pxoLvcvQ2A5V/RN+pHnHHg7dRP259F24SCmhGhoIHwn8ugCEFMutV9MyjAuqfrktCizgcmhBjItLTQTVLUWTctEP2CpLF3CjJx4sWXI0TZNQ++Cgb+y7xkgOSujK99xpWfM1hwz
+export AWS_DEFAULT_REGION=us-east-1
+export POSTGRES_USERNAME=myusername
+export POSTGRES_PASSWORD=mypasswork
+export POSTGRES_HOST=cdr.cfmcmogtn31k.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=mybucket742483543256
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
